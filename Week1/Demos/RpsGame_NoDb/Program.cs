36ee625af295e920f73da98f0e727bde41363e07
Joshua Raneri <raneri.josh@gmail.com>
2020-12-16 12:39:13 -0500

    catchup

--- Week1/Demos/RpsGame_NoDb/Program.cs
+++ Week1/Demos/RpsGame_NoDb/Program.cs
@@ -6,7 +6,24 @@ namespace RpsGame_NoDb
     {
         static void Main(string[] args)
         {
-            
+            bool userResponseParsed;
+            int number;
+            do{
+                Console.WriteLine("Please make a selection");
+                Console.WriteLine("  1. Rock");
+                Console.WriteLine("  2. Paper");
+                Console.WriteLine("  3. Scissors");
+                string userResponse = Console.ReadLine();
+                userResponseParsed = int.TryParse(userResponse, out number);
+                if (userResponseParsed == false || (number < 1 || number > 3)){
+                    Console.WriteLine("Invalid input.");
+                }
+            } while(userResponseParsed == false || (number < 1 || number > 3));
+            switch (number)
+            {
+
+            }
+
         }
     }
 }
