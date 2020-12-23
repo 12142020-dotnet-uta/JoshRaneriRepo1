dadb3f2475a5934f371a0086ab5c00203fa42865
Joshua Raneri <raneri.josh@gmail.com>
2020-12-23 10:52:20 -0500

    .

--- P0_JoshRaneri/Customer.cs
+++ P0_JoshRaneri/Customer.cs
@@ -1,7 +1,11 @@
+using System;
+
 namespace P0_JoshRaneri
 {
     public class Customer
     {
-        
+        private Guid customerId = Guid.NewGuid();
+        [Key]
+        public Guid CustomerId { get { return }}
     }
 }
